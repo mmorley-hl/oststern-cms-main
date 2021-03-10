b683d1a823cf63929b7702ed11dad370555052be
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-03-10 10:31:19 +0100

    cleaned up

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,7 @@
 import '../css/index.scss';
-// import './_cursor.js';
-// import './_test.js';
-// import './_header.js';
+import './_cursor.js';
+import './_test.js';
+import './_header.js';
 import './_mapbox.js';
 import './_color_switch.js';
 import './_wow.js';
