94d23d32233c17d5155686eda57329209a9c200f
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-03-11 10:48:00 +0100

    updated map

--- src/assets/js/_mapbox.js
+++ src/assets/js/_mapbox.js
@@ -102,6 +102,7 @@ var map = new mapboxgl.Map({
 
 //disable zoom on map
 map.scrollZoom.disable();
+if (map.tap) map.tap.disable();
 
 //added basich control on map
 map.addControl(new mapboxgl.NavigationControl());
@@ -234,21 +235,21 @@ map.on('click', 'places', function (e) {
 	// copies of the feature are visible, the popup appears
 	// over the copy being pointed to.
 	while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-	coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+		coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 	}
 	 
 	new mapboxgl.Popup()
 	.setLngLat(coordinates)
 	.setHTML(description)
 	.addTo(map);
-	});
+});
 	 
-	// Change the cursor to a pointer when the mouse is over the places layer.
-	map.on('mouseenter', 'places', function () {
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'places', function () {
 	map.getCanvas().style.cursor = 'pointer';
-	});
-	 
-	// Change it back to a pointer when it leaves.
-	map.on('mouseleave', 'places', function () {
+});
+	
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'places', function () {
 	map.getCanvas().style.cursor = '';
-	});
+});
