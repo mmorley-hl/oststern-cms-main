68a11e989f320166cab41b67fa2669fe1d88ba26
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-04-12 15:05:32 +0200

    updated oststern

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,7 +6,7 @@ import './_cursor.js';
 import './_header.js';
 import './_mapbox.js';
 import './_color_switch.js';
-// import './_wow.js';
+import './_wow.js';
 import './_scrollAnker.js';
 import './_carousel.js';
 import './_toTopButton.js';
