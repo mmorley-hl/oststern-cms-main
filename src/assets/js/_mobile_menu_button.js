1baa713404a2c8b1ba160a8045d15641d8dcff91
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-04-15 16:42:57 +0200

    updated mobile menu button

--- src/assets/js/_mobile_menu_button.js
+++ src/assets/js/_mobile_menu_button.js
@@ -31,3 +31,9 @@ mobileMenuButton.forEach(elem => {
         console.log('mobile_button', elem)
     }
 });
+
+var menuButton = document.getElementById('menuButton');
+menuButton.addEventListener('click', function (e) {
+    menuButton.classList.toggle('is-active');
+    e.preventDefault();
+});
