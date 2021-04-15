bb25ce9bd016fc172162ee50f611be163bbaffee
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-04-15 12:51:45 +0200

    added mobile menu prototyp

--- src/assets/js/_mobile_menu_button.js
+++ src/assets/js/_mobile_menu_button.js
@@ -5,19 +5,25 @@ const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 const mobileMenuButton = document.querySelectorAll('.mobile_menu_button')
 const mobileMenu = document.querySelectorAll('.mobile_menu')
 console.log('mobile_button', mobileMenuButton)
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 mobileMenuButton.forEach(elem => {
     
     elem.onclick = () => {
         mobileMenu.forEach(elem => {
             console.log('style', elem.style.top)
             if (elem.style.top == '') {
-                // disableBodyScroll(elem);
+                disableBodyScroll(elem);
                 elem.style.top = "0";
             } else if (elem.style.top == '0px') {
-                // enableBodyScroll(elem);
-                elem.style.top = "-100vh";
+                enableBodyScroll(elem);
+                elem.style.top = "-140vh";
             } else {
-                // disableBodyScroll(elem);
+                disableBodyScroll(elem);
                 elem.style.top = "0";
             }
 
