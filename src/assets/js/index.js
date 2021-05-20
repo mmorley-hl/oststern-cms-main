a5a060fc898b37ba57afd6eab446a8ce81161e0c
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-05-20 14:37:05 +0200

    updated projeckt

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,6 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 // import css for webpack
 import '../css/index.scss';
 // javscript imports
@@ -24,4 +27,31 @@ new simpleParallax(image);
 // new simpleParallax(image, {
 // 	scale: 1.5
 // });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".panel:not(:last-child)", {
+  yPercent: -100, 
+  ease: "none",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#container",
+    start: "top top",
+    end: "+=300%",
+    scrub: true,
+    pin: true
+  }
+});
+
+
+gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+
+let red_panel = document.querySelector('.red')
+
+let red_inner = document.querySelector('.panel_inner')
+// let rectCollection = red_inner.getClientRects();
+const rect = red_inner.getBoundingClientRect();
+console.log(`height: ${rect.height}`);
+
+red_panel.style.height = (rect.height + 200) + 'px';
 
