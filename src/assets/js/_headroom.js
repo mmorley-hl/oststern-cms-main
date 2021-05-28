3f2a6269554003d57b1b8ffaddfd81704d21f91b
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-05-28 10:02:38 +0200

    updated core _navigation

--- src/assets/js/_headroom.js
+++ src/assets/js/_headroom.js
@@ -1,11 +1,15 @@
 // if you're using a bundler, first import:
 import Headroom from "headroom.js";
+
 // grab an element
 var elems = document.querySelectorAll('.header')
 // var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
 elems.forEach((elem) => {
-    var headroom  = new Headroom(elem);
+    var headroom = new Headroom(elem, {
+        "offset": 900,
+        "tolerance": 11
+      });
     // initialise
     headroom.init();
 })
