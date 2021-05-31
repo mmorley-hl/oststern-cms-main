7cb6a301232c90e3c2977851e1006cbfaae98d95
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-05-31 19:35:42 +0200

    updatdd tabs

--- src/assets/js/_tabs_maps.js
+++ src/assets/js/_tabs_maps.js
@@ -15,10 +15,10 @@ paris.forEach((elem) => {
     
 })
 
-document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").click();
 
 function openCity(evt, cityName) {
-
+    if (cityName == 'London') {
     // Declare all variables
     var i, tabcontent, tablinks;
   
@@ -35,7 +35,31 @@ function openCity(evt, cityName) {
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById('London').style.display = "block";
+    document.getElementById('Paris').style.display = "none";
     evt.currentTarget.className += " active";
+    }
+    if (cityName == 'Paris') {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById('Paris').style.display = "block";
+        document.getElementById('London').style.display = "none";
+        evt.currentTarget.className += " active";
+        }
+
   }
 
