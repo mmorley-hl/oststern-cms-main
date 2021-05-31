04bc00ad4edfe8b9791c48b8eb06b8ed48f4dd3a
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-05-31 12:56:36 +0200

    updated footer

--- src/assets/js/_toTopButton.js
+++ src/assets/js/_toTopButton.js
@@ -1,30 +1,29 @@
-const elems = document.querySelectorAll('.to_top_button')
+const elems = document.querySelectorAll('.to_top_button_logo')
 
 elems.forEach((elem) => {
+	console.log('logo')
 	elem.onclick = function(e) {
-		e.preventDefault()
-
 		window.scrollTo({
 			top: 0,
 			behavior: 'smooth'
 		})
 	}
 })
-const buttonElems = document.querySelectorAll('.button_test')
+// const buttonElems = document.querySelectorAll('.button_test')
 
-window.onscroll = (e) => {
-	// console.log(window.pageYOffset)
-	if (window.pageYOffset > "650") {
-		// console.log('button scroll 650 plus')
-		buttonElems.forEach((elem) => {
-			// console.log("button", elem )
-			elem.classList.remove("button_test--hide");
-		})
-	} else {
-		console.log('button scroll 650 minus')
-		buttonElems.forEach((elem) => {
-			// console.log("button", elem )
-			elem.classList.add("button_test--hide");
-		})
-	}
-}
+// window.onscroll = (e) => {
+// 	// console.log(window.pageYOffset)
+// 	if (window.pageYOffset > "650") {
+// 		// console.log('button scroll 650 plus')
+// 		buttonElems.forEach((elem) => {
+// 			// console.log("button", elem )
+// 			elem.classList.remove("button_test--hide");
+// 		})
+// 	} else {
+// 		console.log('button scroll 650 minus')
+// 		buttonElems.forEach((elem) => {
+// 			// console.log("button", elem )
+// 			elem.classList.add("button_test--hide");
+// 		})
+// 	}
+// }
